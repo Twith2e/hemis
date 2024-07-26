fde565c1910c99e1c4ff2f04f7dde15468404299
twith2e <abdulbasitadebajo1619@gmail.com>
2024-07-26 10:34:40 +0100

    last update

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ menuBtn.addEventListener("click", () => {
 sideBarCloseBtn.addEventListener("click", () => {
   sideBar.style.right = "-100%";
 });
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+  let currentScrollTop = document.documentElement.scrollTop;
+
+  if (currentScrollTop > lastScrollTop) {
+    navToTop.style.visibility = "hidden";
+  } else {
+    navToTop.style.visibility = "visible";
+    console.log("Scrolling up");
+  }
+
+  lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
+});
+
+navToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
